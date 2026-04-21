7f8e6fd0608c42246d95cc3ca990c6a9ccafa040
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-04-21 18:14:01 +0530

    TE-T492: Implemented ListView

--- Demo3Crecyclerviewadapter/app/src/main/java/com/example/demo3crecyclerviewadapter/RecyclerViewPracActivity.kt
+++ Demo3Crecyclerviewadapter/app/src/main/java/com/example/demo3crecyclerviewadapter/RecyclerViewPracActivity.kt
@@ -1,12 +1,25 @@
 package com.example.demo3crecyclerviewadapter
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewPracActivity : AppCompatActivity() {
+
+    val myData = listOf("Kotlin", "Java", "Python", "JavaScript", "C++",
+        "Ruby", "Swift", "Go", "Rust", "PHP",
+        "TypeScript", "Scala", "Kotlin", "Java", "Python", // Includes duplicates
+        "C#", "Dart", "Lua", "Perl", "R",
+        "Kotlin", "Java", "Python", "JavaScript", "C++")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,13 @@ class RecyclerViewPracActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MyAdapter(myData)
     }
 }
+
+c
