503f01b30e99711bc3c546da3f168320245d9c02
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-07-07 18:58:00 +0530

    TE-T729: Implemented wrapper data class TodosResponse.kt and serialized name in UserTask.kt.

--- Demo-5-mvc-architecture/app/src/main/java/com/example/demo5mvcarchitecture/todos/model/UserTask.kt
+++ Demo-5-mvc-architecture/app/src/main/java/com/example/demo5mvcarchitecture/todos/model/UserTask.kt
@@ -1,8 +1,12 @@
 package com.example.demo5mvcarchitecture.todos.model
 
+import com.google.gson.annotations.SerializedName
 data class UserTask(
+    @SerializedName("id")
     val taskId: Int?,
     val userId: Int?,
+    @SerializedName("todo")
     val taskTitle: String?,
+    @SerializedName("completed")
     val isCompleted: Boolean?
 )
