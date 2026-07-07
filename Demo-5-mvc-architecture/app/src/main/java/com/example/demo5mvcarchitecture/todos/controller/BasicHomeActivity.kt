128a13821331849ae1b1a02352a22dd701f9ae22
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-07-07 19:00:39 +0530

    TE-T729: Implemented netwrok calling function in BasicHomeActivity.kt.

--- Demo-5-mvc-architecture/app/src/main/java/com/example/demo5mvcarchitecture/todos/controller/BasicHomeActivity.kt
+++ Demo-5-mvc-architecture/app/src/main/java/com/example/demo5mvcarchitecture/todos/controller/BasicHomeActivity.kt
@@ -1,18 +1,25 @@
 package com.example.demo5mvcarchitecture.todos.controller
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.demo5mvcarchitecture.R
 import com.example.demo5mvcarchitecture.databinding.ActivityBasicHomeBinding
+import com.example.demo5mvcarchitecture.todos.model.TodosModel
 import com.example.demo5mvcarchitecture.todos.view.TodosItemDecor
 import com.example.demo5mvcarchitecture.todos.view.TodosListAdapter
+import kotlinx.coroutines.launch
 
 class BasicHomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBasicHomeBinding
+    private val adapter by lazy {
+        TodosListAdapter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,23 +32,31 @@ class BasicHomeActivity : AppCompatActivity() {
             insets
         }
         setupUi()
+        fetchTasks()
     }
-
-
     // Functions which help to set up UI
     private fun setupUi() {
         setupTodosRecyclerView()
     }
-
     private fun setupTodosRecyclerView() {
         binding.rvTodos.apply {
             layoutManager = LinearLayoutManager(this@BasicHomeActivity)
-            adapter = TodosListAdapter()
+            adapter = this@BasicHomeActivity.adapter
             addItemDecoration(TodosItemDecor(32))
         }
     }
-
     private fun fetchTasks() {
-
+        lifecycleScope.launch {
+            try {
+                val userTasks = TodosModel.fetchTask()
+                adapter.submitList(userTasks)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    this@BasicHomeActivity,
+                    e.localizedMessage,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
