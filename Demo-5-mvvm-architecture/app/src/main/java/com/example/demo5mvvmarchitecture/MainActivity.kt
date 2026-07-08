05bdb2d5861d9830c8b80755aadeaa2600a2e475
Bhumik91 <poshiyabhumik@gmail.com>
2026-07-08 06:41:37 +0530

    TE-T709: Implemented basic screen for choosing the project.

--- Demo-5-mvvm-architecture/app/src/main/java/com/example/demo5mvvmarchitecture/MainActivity.kt
+++ Demo-5-mvvm-architecture/app/src/main/java/com/example/demo5mvvmarchitecture/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.demo5mvvmarchitecture
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.demo5mvvmarchitecture.auth.login.view.LoginActivity
+import com.example.demo5mvvmarchitecture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setupOnClicks()
+    }
+    private fun setupOnClicks() {
+        findViewById<Button>(R.id.btnTodos).setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
