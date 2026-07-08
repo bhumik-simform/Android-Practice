84e8030c2d5b29e158f12a25cb260fb831b32043
Bhumik91 <poshiyabhumik@gmail.com>
2026-07-08 06:42:01 +0530

    TE-T709: Implemented basic screen for choosing the project.

--- Demo-5-mvvm-architecture/app/src/main/java/com/example/demo5mvvmarchitecture/MainActivity.kt
+++ Demo-5-mvvm-architecture/app/src/main/java/com/example/demo5mvvmarchitecture/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
 import com.example.demo5mvvmarchitecture.auth.login.view.LoginActivity
-import com.example.demo5mvvmarchitecture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
