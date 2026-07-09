e7a2a4f6b35e5bdc5b0050e204090df502dbdc6e
Bhumik91 <poshiyabhumik@gmail.com>
2026-07-09 23:55:08 +0530

    TE_729: Initialize project for understanding mvp architecture

---
+++ Demo-5-MVP-Architecture/app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.demo5mvparchitecture"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "com.example.demo5mvparchitecture"
+        minSdk = 24
+        //noinspection OldTargetApi
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+}
